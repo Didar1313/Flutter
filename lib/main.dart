3600ac89f3acf7e34b54eecc39047c9442481a46
IB Didar <73778140+Didar1313@users.noreply.github.com>
2024-04-30 19:31:06 +0600

    RowColumn with Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'Controller.dart';
+import 'Row_Demo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,19 +9,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Controller(),
+      home: Row_Demo(),
     );
   }
 }
+
+
 
